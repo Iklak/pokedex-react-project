4e862a28c259ec0c4170c3e6c5c1cb9893120c64
Iklakh Hussain <101625077+Iklak@users.noreply.github.com>
2025-01-24 17:30:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Pokedex from './component/pokedex/Pokedex';
+import Search from './component/search/Search';
+
+function App() {
+  
+
+  return (
+  
+   <>
+   <Pokedex></Pokedex>
+   
+   </>
+  
+  );
+}
+
+export default App
